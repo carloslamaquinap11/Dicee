70247438cfedeb2b8f5e444e3b8870a996624e8c
Carlos Ponce <jose.ponce3@unmsm.edu.pe>
2023-09-10 17:24:30 -0500

    First commit dices

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var dicesList = [
+        "DiceOne",
+        "DiceTwo",
+        "DiceThree",
+        "DiceFour",
+        "DiceFive",
+        "DiceSix"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageView1.image = UIImage(named: dicesList.randomElement()!)
+        diceImageView2.image = UIImage(named: dicesList.randomElement()!)
+        
+    }
+    
 }
 
